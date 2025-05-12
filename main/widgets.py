b0cbf6ae308857e0b0eac6138a4816b9ae8ca87e
Pastierno <marcopatierno.m@gmail.com>
2025-05-12 13:04:27 +0200

    last push

--- main/widgets.py
+++ main/widgets.py
@@ -1,31 +1,31 @@
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 class AnimatedButton(QtWidgets.QPushButton):
-    def __init__(self, *args, glow_color=QtGui.QColor("#88C0D0"), max_blur=30, **kwargs):
+    def __init__(self, *args, glow_color=QtGui.QColor("#F55555"), max_blur=20, **kwargs):
         super().__init__(*args, **kwargs)
-        # effetto glow esterno
+        # Effetto glow esterno
         self._effect = QtWidgets.QGraphicsDropShadowEffect(self)
         self._effect.setOffset(0, 0)
         self._effect.setColor(glow_color)
         self._effect.setBlurRadius(0)
         self.setGraphicsEffect(self._effect)
 
-        # animazione bloom all’entrata
+        # Animazione bloom all’entrata
         self._enter_anim = QtCore.QPropertyAnimation(self._effect, b"blurRadius", self)
-        self._enter_anim.setDuration(300)
+        self._enter_anim.setDuration(200)
         self._enter_anim.setStartValue(0)
         self._enter_anim.setEndValue(max_blur)
 
-        # animazione bloom all’uscita
+        # Animazione bloom all’uscita
         self._leave_anim = QtCore.QPropertyAnimation(self._effect, b"blurRadius", self)
-        self._leave_anim.setDuration(300)
+        self._leave_anim.setDuration(200)
         self._leave_anim.setStartValue(max_blur)
         self._leave_anim.setEndValue(0)
 
-        # animazione colore glow (opzionale)
+        # Animazione del colore del glow
         self._color_anim = QtCore.QPropertyAnimation(self._effect, b"color", self)
-        self._color_anim.setDuration(300)
-        self._color_anim.setStartValue(glow_color.lighter(150))
+        self._color_anim.setDuration(200)
+        self._color_anim.setStartValue(glow_color.lighter(120))
         self._color_anim.setEndValue(glow_color)
 
     def enterEvent(self, event):
